c87893a4ffa8ca51eb4073330c41e5558140bcc4
rennan-aquino <rennan.silva@bandtec.com.br>
2021-07-18 13:33:34 -0300

    skeleton and features

---
+++ calculator/Assets/js/main.js
@@ -0,0 +1,21 @@
+function calcNumbers(result){
+    document.querySelector('.area').value += result;
+} 
+function percentage(result){
+    let result1 = String(result);
+    if(result1.includes('%')){
+        let a, b, percentage;
+        let splitWord = [];
+        a = Number(splitWord[0]);
+        b = Number(splitWord[1]);
+        percent = (a/100 * b);
+        document.querySelector('.area').value = percent;
+    }
+}
+function makeNegative(result) {
+    if(result.charAt(0) =='_'){
+        document.querySelector('.area').value = result.replace('-','');
+    } else {
+        document.querySelector('.area').value = '-' + result;
+    }
+}
